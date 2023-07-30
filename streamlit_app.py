188aea647a357bf4669b375642814ceaf606ba78
madascicom <137607688+madascicom@users.noreply.github.com>
2023-07-30 20:02:22 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,57 @@
 import streamlit as st
 from llamaapi import LlamaAPI
 import json
+import os
 
+# App title
+st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
 
 st.title('🦙 Llama Chat Test')
 
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = []
-    
-llama = LlamaAPI('Llama_API_Token')
 
-def get_bot_response(prompt):
-    response = llama.generate(prompt, max_tokens=100, temperature=0.5, top_p=0.3)
-    return response.text
 
-user_input = get_user_input() 
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
 
-if user_input:
-    output = get_bot_response(user_input)
-    
-    st.session_state.generated.append({"role":"user", "content":user_input})
-    st.session_state.generated.append({"role":"assistant", "content":output})
+# Display or clear chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+def clear_chat_history():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+
+# Function for generating LLaMA2 response
+# Refactored from <https://github.com/a16z-infra/llama2-chatbot>
+def generate_llama2_response(prompt_input):
+    string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
+    for dict_message in st.session_state.messages:
+        if dict_message["role"] == "user":
+            string_dialogue += "User: " + dict_message["content"] + "\\n\\n"
+        else:
+            string_dialogue += "Assistant: " + dict_message["content"] + "\\n\\n"
+    output = llama.run(input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
+                                  "temperature":0.1, "top_p":0.9, "max_length":512, "repetition_penalty":1})
+    return output
+
+# User-provided prompt
+if prompt := st.chat_input:
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
+
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = generate_llama2_response(prompt)
+            placeholder = st.empty()
+            full_response = ''
+            for item in response:
+                full_response += item
+                placeholder.markdown(full_response)
+            placeholder.markdown(full_response)
+    message = {"role": "assistant", "content": full_response}
+    st.session_state.messages.append(message)
