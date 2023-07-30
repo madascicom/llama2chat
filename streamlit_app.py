0298e6f5ad9fa19772dd222feb70d618373f13dc
madascicom <137607688+madascicom@users.noreply.github.com>
2023-07-30 19:45:16 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,23 @@
 import streamlit as st
+from llamaapi import LlamaAPI
+import json
 
-st.title('🎈 App Name')
 
-st.write('Hello world!')
+st.title('🦙 Llama Chat Test')
+
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+    
+llama = LlamaAPI('Llama_API_Token')
+
+def get_bot_response(prompt):
+    response = llama.generate(prompt, max_tokens=100, temperature=0.5, top_p=0.3)
+    return response.text
+
+user_input = get_user_input() 
+
+if user_input:
+    output = get_bot_response(user_input)
+    
+    st.session_state.generated.append({"role":"user", "content":user_input})
+    st.session_state.generated.append({"role":"assistant", "content":output})
